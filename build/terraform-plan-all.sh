cd0be0ba58d20e6452461b862eae1806ef1ce6a9
Kenneth Radunz <kenneth.radunz@gmail.com>
2022-12-10 20:10:56 +0100

    adds host to plan

--- build/terraform-plan-all.sh
+++ build/terraform-plan-all.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env sh
 
 for dir in /workspace/infrastructure/environments/*/; do
+  env=$(basename dir)
+  host=$(sed "s/${_HOST_PATTERN}/${env}/")
   echo ""
   echo "*************** TERRAFORM INIT & PLAN *******************"
-  echo "Directory : ${dir}"
+  echo "Directory   : ${dir}"
+  echo "Environment : ${env}"
+  echo "Host        : ${host}"
   echo "*********************************************************"
   cd "${dir}" || exit 1
   terraform init || exit 1
-  terraform plan || exit 1
+  terraform plan -var host="${host}" || exit 1
 done
