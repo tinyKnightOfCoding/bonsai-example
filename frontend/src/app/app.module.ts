df81d819c9d96ebc2dd13bb85817e1c04697aa91
Kenneth Radunz <kenneth.radunz@gmail.com>
2022-12-11 16:09:21 +0100

    adds frontend module

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component';
+import {ApiModule, Configuration, ConfigurationParameters} from "../api";
+import {HttpClientModule} from "@angular/common/http";
+
+function apiConfigFactory(): Configuration {
+  const params: ConfigurationParameters = {
+    basePath: '/api',
+  };
+  return new Configuration(params);
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ApiModule.forRoot(apiConfigFactory),
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
